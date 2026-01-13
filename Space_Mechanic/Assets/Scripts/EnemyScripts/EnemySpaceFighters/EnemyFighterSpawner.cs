431f747da8ce0800e26e9b4f30a0ec9d153205d4
Nara-student <nara.chen@elev.ga.lbs.se>
2026-01-13 12:38:41 +0100

    fixat enemy fighter spacecraft

---
+++ Space_Mechanic/Assets/Scripts/EnemyScripts/EnemySpaceFighters/EnemyFighterSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFighterSpawner : MonoBehaviour
+{
+    [Header("Fighter")]
+    [SerializeField] GameObject fighter;
+    [SerializeField] GameObject target;
+    [SerializeField] List<GameObject> orbitPath = new List<GameObject>();
+
+    [Header("Other")]
+    [SerializeField] List<GameObject> spawns = new List<GameObject>();
+    [SerializeField] int squadCount = 2;
+    [SerializeField] float minimumSpawnTime = 10;
+    [SerializeField] float maximumSpawnTime = 30;
+    private float nextSpawn;
+
+    void Start()
+    {
+        nextSpawn = Time.time + Random.Range(minimumSpawnTime, maximumSpawnTime + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextSpawn)
+        {
+            SpawnFighter(spawns[Random.Range(0, spawns.Count)]);
+            nextSpawn = Time.time + Random.Range(minimumSpawnTime, maximumSpawnTime + 1);
+        }
+    }
+
+    private void SpawnFighter(GameObject spawn)
+    {
+        for(int i = 0; i < squadCount; i++)
+        {
+            GameObject fighterClone = Instantiate(fighter, new Vector2(spawn.transform.position.x + i * 2, spawn.transform.position.y + i * 2), Quaternion.identity);
+            fighterClone.GetComponent<EnemyFighterAI>().AssignTarget(target);
+            fighterClone.GetComponent<EnemyFighterAI>().AssignOrbitPath(orbitPath);
+        }
+    }
+}
