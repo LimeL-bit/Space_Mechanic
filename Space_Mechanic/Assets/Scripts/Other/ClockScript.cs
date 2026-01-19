813088adc31cabedcec078ea03439c1ae154e9fb
William2057 <Williamarnell882@gmail.com>
2026-01-19 14:24:13 +0100

    Added Timer To Planet Platformer

--- Space_Mechanic/Assets/Scripts/Other/ClockScript.cs
+++ Space_Mechanic/Assets/Scripts/Other/ClockScript.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
 using TMPro;
-using System;
-
-public class GameTimer : MonoBehaviour
+using UnityEngine.SceneManagement; // Required for TextMeshPro
+public class Timer : MonoBehaviour
 {
-    public TMP_Text timerText;
+    public float timeRemaining = 120; // 2 minutes in seconds
+    public bool timerIsRunning = false;
+    public TMP_Text timeText; // Drag your TextMeshPro object here
+
+    private void Start()
+    {
+        // Start the timer automatically
+        timerIsRunning = true;
+    }
 
     void Update()
     {
-        float t = Time.timeSinceLevelLoad;
-        TimeSpan timeSpan = TimeSpan.FromSeconds(t);
-        timerText.text = timeSpan.ToString(@"mm\:ss");
+        GetComponent<PlayerHealth>();
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                DisplayTime(0);
+                SceneManager.LoadScene(6);
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        // Formatting seconds into MM:SS
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
-
--- Space_Mechanic/Assets/Scripts/PlayerScripts/PlayerHealth/Player Armour.cs
+++ Space_Mechanic/Assets/Scripts/PlayerScripts/PlayerHealth/Player Armour.cs
@@ -11,24 +11,12 @@ public class ArmorPickup : MonoBehaviour
             PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
-                Transform visual = other.transform.Find("ArmourVisual");
-
-                if (visual != null)
-                {
-                    StartCoroutine(ShowArmorTemporarily(visual, 5f));
-                }
                 playerHealth.isArmored = true;
                 isPickedUp = true;
                 Debug.Log("Armor equipped!");
                 Destroy(gameObject);
             }
         }
-    }
-    private IEnumerator ShowArmorTemporarily(Transform armorVisual, float duration)
-    {
-        armorVisual.gameObject.SetActive(true);  
-        yield return new WaitForSeconds(duration); 
-        armorVisual.gameObject.SetActive(false); 
     }
 }
 
